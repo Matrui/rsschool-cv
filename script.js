a8ecdfc8af18ee018ca505b59e01dd312e94afa7
Matrui <yanmatr@gmail.com>
2023-06-24 14:33:50 +0200

    asice added

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+"use strict"
+let firstPlayerNumber = 0;
+let secondPlayerNumber = 0;
+let timer = 100;
 
-// const sum = (a, b) => { return a + b };
-// console.log(sum(1, 3));
-// //
-// const sum = {a, b} => a + b;
-//console.log(sum(1, 3));
-// const sum = a, b => a + b
+
+function diceRoll() {
+		firstPlayerNumber = Math.floor((Math.random() * 6) + 1);
+        secondPlayerNumber = Math.floor((Math.random() * 6) + 1);
+        document.getElementById("diceImage1");
+        diceImage1.src = "images/svg/dice" + firstPlayerNumber + ".svg";
+        diceImage1.alt = firstPlayerNumber;
+
+        document.getElementById("diceImage2");
+        diceImage2.src = "images/svg/dice" + secondPlayerNumber + ".svg";
+        diceImage2.alt = secondPlayerNumber;
+
+        if(firstPlayerNumber > secondPlayerNumber){
+            document.getElementById("diceFirstPlayer");
+            diceFirstPlayer.style.backgroundColor = "aqua";
+            document.getElementById("diceSecondPlayer");
+            diceSecondPlayer.style.backgroundColor = "rgba(51, 58, 68, 0.234)";
+        }else if(firstPlayerNumber < secondPlayerNumber){
+            document.getElementById("diceFirstPlayer");
+            diceFirstPlayer.style.backgroundColor = "rgba(51, 58, 68, 0.234)";
+            document.getElementById("diceSecondPlayer");
+            diceSecondPlayer.style.backgroundColor = "aqua";
+        }else{
+            document.getElementById("diceFirstPlayer");
+            diceFirstPlayer.style.backgroundColor = "aqua";
+            document.getElementById("diceSecondPlayer");
+            diceSecondPlayer.style.backgroundColor = "aqua";
+        }
+}
+
+document.getElementById("rollButton").addEventListener("click", diceRoll);
+
+// function diceShaker(){
+//         timer = timer + 100;
+//         diceRoll();
+//         if(timer <= 7000){
+//         setTimeout(diceRoll, timer);
+//         }
+// }
